0a27e1fd2c428aa2906740c5ae5c96b94f9fd703
Chuck Haines <crono1023@gmail.com>
2017-01-31 14:40:24 -0500

    Added new file main.cpp containing the main code bin/atm-example is the finished compiled program binary

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* Main.cpp Main function for atm-example program.
+** This is the main file for the atm-example.
+*/
+
+//Includes
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Test successful!\n";
+	return 0;
+}
